e088253a26d7fb85af9f73916c33be1da4840ac1
hyerimhan <hnhyerim@gmail.com>
2023-01-13 02:59:42 +0900

    feat: Login & Logout

---
+++ src/api/firebase.js
@@ -0,0 +1,38 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+
+// 로그인
+export function login() {
+  signInWithPopup(auth, provider).catch(console.error);
+}
+
+// 로그아웃
+export function logout() {
+  signOut(auth).catch(console.error);
+}
+
+// 로그인 유지
+export function onUserStateChange(callback) {
+  onAuthStateChanged(auth, (user) => {
+    callback(user);
+  });
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { FiShoppingBag } from "react-icons/fi";
 import { BsFillPencilFill } from "react-icons/bs";
+import { login, logout, onUserStateChange } from "../api/firebase";
+import User from "./User";
 
 export default function Header() {
+  const [user, setUser] = useState();
+
+  // 로그인 상태인지 아닌지 확인
+  useEffect(() => {
+    // 인자가 동일하면 참조 값(setUser)만 전달해줘도 된다.
+    // onUserStateChange(setUser);
+    onUserStateChange((user) => {
+      setUser(user);
+    });
+  }, []);
+
   return (
     <header className="flex justify-between border-b border-gray-300 p-2">
       <Link to="/" className="flex items-center text-4xl text-brand">
@@ -16,7 +29,9 @@ export default function Header() {
         <Link to="/products/new" className="text-2xl">
           <BsFillPencilFill />
         </Link>
-        <button>Login</button>
+        {user && <User user={user} />}
+        {!user && <button onClick={login}>Login</button>}
+        {user && <button onClick={logout}>Logout</button>}
       </nav>
     </header>
   );
