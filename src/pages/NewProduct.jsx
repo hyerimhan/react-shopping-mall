dab68be1275786fe9dda729cd023c6ad029ef457
hyerimhan <hnhyerim@gmail.com>
2023-01-17 01:23:54 +0900

    feat: useMutate

--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
-import { addNewProduct } from "../api/firebase";
 import { uploadImage } from "../api/uploader";
 import Button from "../components/ui/Button";
+import useProducts from "../hooks/useProducts";
 
 export default function NewProduct() {
   const [product, setProduct] = useState({});
   const [file, setFile] = useState();
   const [isUploading, setIsUploading] = useState(false);
   const [success, setSuccess] = useState();
+  const { addProduct } = useProducts();
 
   const handleChange = (e) => {
     const { name, value, files } = e.target;
@@ -27,13 +28,20 @@ export default function NewProduct() {
     // 제품의 사진을 Cloudyinary에 업로드하고 URL을 획득
     uploadImage(file) //
       .then((url) => {
+        addProduct.mutate(
+          // Firebase에 새로운 제품을 추가함
+          { product, url },
+          {
+            onSuccess: () => {
+              setSuccess("성공적으로 제품이 추가되었습니다.");
+              setTimeout(() => {
+                setSuccess(null);
+              }, 4000);
+            },
+          }
+        );
+
         console.log(url);
-        // Firebase에 새로운 제품을 추가함
-        addNewProduct(product, url) //
-          .then(() => setSuccess("성공적으로 제품이 추가되었습니다."));
-        setTimeout(() => {
-          setSuccess(null);
-        }, 4000);
       })
       .finally(() => setIsUploading(false));
   };
